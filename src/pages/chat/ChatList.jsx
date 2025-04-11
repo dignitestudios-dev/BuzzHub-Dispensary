46d7e8a46bb4da17d85bd800f68cb6202842fd84
evansmith14 <184949993+evansmith14@users.noreply.github.com>
2025-04-11 06:10:32 -0700

    added no chats yet when there are no chats

--- src/pages/chat/ChatList.jsx
+++ src/pages/chat/ChatList.jsx
@@ -32,45 +32,51 @@ const ChatList = ({
   }, [chats]);
   return (
     <ul className="space-y-2 overflow-auto">
-      {chats?.map((chat) => (
-        <li
-          key={chat?.id}
-          className={`flex items-center justify-between p-4 rounded-lg transition duration-200 cursor-pointer border ${
-            selectedChat?.id == chat?.id
-              ? "border-[#1D7C42] bg-[#1D7C4215]"
-              : "bg-gray-100"
-          }`}
-          onClick={() => {
-            setSelectedChat(chat);
-            unreadCounts[chat?.id] > 0 && markMessagesAsRead(chat?.id, userId);
-          }} // Add onClick to navigate
-        >
-          <div className="flex items-center space-x-4">
-            {chat?.image ? (
-              <img
-                src={chat?.image}
-                className="w-12 h-12 rounded-full object-scale-down border bg-gray-50"
-              />
-            ) : (
-              <div className="w-12 h-12 rounded-full bg-gray-300"></div>
-            )}
-            <div>
-              <p className="text-lg font-semibold text-[#074F57]">
-                {chat?.name || "N/A"}
-              </p>
-              <p className="text-sm font-medium text-gray-600">
-                {chat?.last_msg?.content || ""}
-              </p>
-            </div>
-          </div>
-          {unreadCounts[chat?.id] > 0 && (
-            <span className="unread-badge bg-green-500 text-white text-xs w-7 h-6 rounded-full flex items-center justify-center">
-              {unreadCounts[chat?.id]}
-            </span>
-          )}
-          {/* <span className="text-sm text-gray-400">{chat.date}</span> */}
+      {chats.length === 0 ? (
+        <li className="text-gray-500 text-center py-4 text-xl font-semibold">
+          No chats yet
         </li>
-      ))}
+      ) : (
+        chats.map((chat) => (
+          <li
+            key={chat?.id}
+            className={`flex items-center justify-between p-4 rounded-lg transition duration-200 cursor-pointer border ${
+              selectedChat?.id == chat?.id
+                ? "border-[#1D7C42] bg-[#1D7C4215]"
+                : "bg-gray-100"
+            }`}
+            onClick={() => {
+              setSelectedChat(chat);
+              unreadCounts[chat?.id] > 0 &&
+                markMessagesAsRead(chat?.id, userId);
+            }}
+          >
+            <div className="flex items-center space-x-4">
+              {chat?.image ? (
+                <img
+                  src={chat?.image}
+                  className="w-12 h-12 rounded-full object-scale-down border bg-gray-50"
+                />
+              ) : (
+                <div className="w-12 h-12 rounded-full bg-gray-300"></div>
+              )}
+              <div>
+                <p className="text-lg font-semibold text-[#074F57]">
+                  {chat?.name || "N/A"}
+                </p>
+                <p className="text-sm font-medium text-gray-600">
+                  {chat?.last_msg?.content || ""}
+                </p>
+              </div>
+            </div>
+            {unreadCounts[chat?.id] > 0 && (
+              <span className="unread-badge bg-green-500 text-white text-xs w-7 h-6 rounded-full flex items-center justify-center">
+                {unreadCounts[chat?.id]}
+              </span>
+            )}
+          </li>
+        ))
+      )}
     </ul>
   );
 };
