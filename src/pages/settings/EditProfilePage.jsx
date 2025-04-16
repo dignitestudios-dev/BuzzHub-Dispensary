327f201a2bf959a160cf8e9a31788fcca466f482
evansmith14 <184949993+evansmith14@users.noreply.github.com>
2025-04-16 05:32:17 -0700

    fixing coordinates

--- src/pages/settings/EditProfilePage.jsx
+++ src/pages/settings/EditProfilePage.jsx
@@ -222,7 +222,10 @@ const EditProfilePage = () => {
       data.append("zipCode", formData.zipCode);
       data.append("disType", formData.disType);
 
-      data.append("location[coordinates]", JSON.stringify(latLong));
+      data.append(
+        "location[coordinates]",
+        JSON.stringify(originCoords ? originCoords : latLong)
+      );
       // data.append("location[type]", "Point");
 
       // Append file data (if exists)
