f27a27f617537c6a7c5266c0eec5233f62fa753f
evansmith14 <184949993+evansmith14@users.noreply.github.com>
2025-08-11 07:27:56 -0700

    fixed setuserdata null

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -26,6 +26,7 @@ const AuthProvider = ({ children }) => {
 
   const signOut = () => {
     setToken(null);
+    setUserData(null)
     setIsAuthenticated(false);
     localStorage.clear();
     sessionStorage.clear();
